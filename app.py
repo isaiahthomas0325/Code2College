b92f8119caac7fd694cdece12e49024a6e1f9ad4
Isaiah Thomas <soccerboyisaiah035@gmail.com>
2026-05-27 13:31:50 -0500

    Added form submission for notes input

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    notes = ""
+
+    if request.method == "POST":
+        notes = request.form.get("notes")
+
+    return render_template("index.html", notes=notes)
 
 if __name__ == "__main__":
     app.run(debug=True)
