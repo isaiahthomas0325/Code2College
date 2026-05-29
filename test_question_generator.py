e2a0280806c293410bc00ad25268c349f683af71
Isaiah Thomas <soccerboyisaiah035@gmail.com>
2026-05-29 18:04:44 -0500

    Added test cases for question generator

---
+++ test_question_generator.py
@@ -0,0 +1,10 @@
+from question_generator import generate_questions
+
+# Test 1
+print(generate_questions("Photosynthesis uses sunlight."))
+
+# Test 2
+print(generate_questions("Plants need water. Roots absorb nutrients."))
+
+# Test 3
+print(generate_questions(""))
